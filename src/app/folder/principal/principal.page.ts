5c86ce6aa9b37a145ce4fc2dc6bbdff7333014e0
Alejandro <alexsotog82@gmail.com>
2020-03-24 12:48:16 -0600

    se suben cambios

--- src/app/folder/principal/principal.page.ts
+++ src/app/folder/principal/principal.page.ts
@@ -7,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalPage implements OnInit {
 
+  productos = [
+    {nombre: 'EARTHQUAKE-CDRS', descipcion: 'Guitarra Electrica', imagen: 'http://clientes.hermes-music.com.mx/files/empresa1/productos16sm.png' },
+    {nombre: 'EARTHQUAKE-CSB', descipcion: 'Guitarra Electrica', imagen: 'http://clientes.hermes-music.com.mx/files/empresa1/productos7sm.png' },
+    {nombre: 'GHOST RD', descipcion: 'Guitarra Electrica "vintage"', imagen: 'http://clientes.hermes-music.com.mx/files/empresa1/productos47sm.png' },
+    {nombre: 'BEG-462', descipcion: 'Guitarra Electrica', imagen: 'http://clientes.hermes-music.com.mx/files/empresa1/productos21sm.png' },
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
   suscripcion() {}
+
+  contacto() {}
 
 }
